d56157161dc12ecf8f11b76e0be56dc9b2b1bb4a
Azaka2 <NiseAzaka@outlook.com>
2025-12-19 22:26:46 +0800

    fix(build script):修改了httpcomponents-client的路径，使其指向项目内部

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,9 +50,9 @@ dependencies {
 //        "include" to listOf("*.aar", "*.jar"),
 //        "exclude" to listOf("")
 //    )))
-    implementation(files("../../Notesmaster01/httpcomponents-client-4.5.14-bin/lib/httpclient-osgi-4.5.14.jar"))
-    implementation(files("../../Notesmaster01/httpcomponents-client-4.5.14-bin/lib/httpclient-win-4.5.14.jar"))
-    implementation(files("../../Notesmaster01/httpcomponents-client-4.5.14-bin/lib/httpcore-4.4.16.jar"))
+    implementation(files("../httpcomponents-client-4.5.14-bin/lib/httpclient-osgi-4.5.14.jar"))
+    implementation(files("../httpcomponents-client-4.5.14-bin/lib/httpclient-win-4.5.14.jar"))
+    implementation(files("../httpcomponents-client-4.5.14-bin/lib/httpcore-4.4.16.jar"))
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
